564304407b14216c13496906ac3ce1b7e4e2a3fc
Azarudeen <azarudeen_cloud_1998@capstone-centos-01.us-central1-a.c.galvanic-pulsar-380306.internal>
2023-06-30 09:32:56 +0000

    Build Script edit

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
- Build the Docker image
+#Build the Docker image
 docker build -t myapp .
 
 # Run the Docker container
-docker run -d -p 80:80 --name react-app-container myapp
+# docker run -d -p 80:80 --name react-app-container myapp
