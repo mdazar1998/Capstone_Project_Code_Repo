f8f6b0f72e3d065e49554300c1103537b99915f6
Azarudeen <azarudeen_cloud_1998@capstone-centos-01.us-central1-a.c.galvanic-pulsar-380306.internal>
2023-06-30 07:07:39 +0000

    Build script

--- build.sh
+++ build.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-# Remove any existing Docker image and container
-docker stop react-app-container
-docker rm react-app-container
-docker rmi react-app-image
-
-# Build the Docker image
+ Build the Docker image
 docker build -t myapp .
 
 # Run the Docker container
